3e91fa63531d299c024fd6ac619eee8a2ed81ace
TI Sabbir <tisabbir0@gmail.com>
2024-06-23 20:34:58 +0600

    AOS applied in the contact section

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,15 +1,24 @@
+'use client'
 import SectionTitle from "@/components/SectionTitle";
 import { FaLocationArrow, FaMailBulk, FaMap, FaPhone } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
+import Aos from "aos";
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+
 
 const ContactPage = () => {
+
+    useEffect(()=> {
+        Aos.init(    { duration: 1000,} )
+      },[])
     return (
         <div>
             <SectionTitle heading={'Contacts'} subheading={'Stay in touch with me'} />
 
            <div className="flex items-center justify-center gap-8">
-           <div className="flex flex-col text-center p-4 bg-white rounded-xl gap-8 items-center mt-6">
+           <div data-aos='fade-left' className="flex flex-col text-center p-4 bg-white rounded-xl gap-8 items-center mt-6">
                 <div>
                     <FaPhone className="text-6xl text-[#8750F5]" />
                 </div>
@@ -27,7 +36,7 @@ const ContactPage = () => {
                     <p className="text-2xl text-[#8750F5]">tisabbir0@gmail.com</p>
                 </div>
             </div>
-            <div className="flex flex-col text-center p-4 bg-white rounded-xl gap-8 items-center mt-6">
+            <div data-aos='fade-right' className="flex flex-col text-center p-4 bg-white rounded-xl gap-8 items-center mt-6">
                 <div>
                     <FaLocationDot className="text-6xl text-[#8750F5]" />
                 </div>
